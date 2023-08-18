6541644dc84c93c9ce21ba6a3decb085e8241317
Hedy <duej123456@gmail.com>
2023-08-18 12:19:55 +0800

    <fix> 部署圖片、換頁問題

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 
 //front
 import FrontView from "@/views/front/FrontView.vue";
@@ -18,7 +18,7 @@ import AdminLogin from "@/views/admin/AdminLogin.vue";
 import statusStore from "@/stores/statusStore";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     //front
     {
